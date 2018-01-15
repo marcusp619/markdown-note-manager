5955b1284db76ce4d688bf72825043826b0f519d
Mark Pearyer <Marcusp619@gmail.com>
2018-01-15 13:00:37 -0600

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import 'milligram';
-
 import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class Sidebar extends Component {
+
+  save() {
+    console.log(`this is ${this}`);
+  }
+
   render() {
     return (
       <div className="editor column column-25">
-        <button className="button button-clear">+ new note</button>
+        <button onClick={(e) => this.save(e)} className="button button-clear">+ new note</button>
       </div>
     );
   }
